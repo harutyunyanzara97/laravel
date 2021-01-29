2681a65f86e9afdbb22d585b38f9d4964f656517
zara <zarahyan.97@gmail.com>
2021-01-29 18:03:07 +0400

    Functionalities of sign up,sign in,network show categories with their posts,user's profile

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'=>'required|min:3|max:20|string',
+            'per_post_rate' => 'numeric|required',
+            'images' => 'mimes:jpeg,jpg,png,gif|required|max:10000',
+        ];
+    }
+}
