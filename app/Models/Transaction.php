a0d9873cf05931e446df1bb5b57e1589cdc117e4
HappyCoder11 <happycodertest@gmail.com>
2021-02-19 11:31:42 +0400

    Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'amount',
+        'user_id',
+        'to_id',
+        'date'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function charged_user()
+    {
+        return $this->belongsTo(User::class, 'to_id');
+    }
+}
